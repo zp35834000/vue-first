3688e3ba99ad3986f0877e40101b44f88216856d
zp <zhao_4365637@163.com>
2018-03-29 15:10:50 +0800

    初次上传

---
+++ src/router/index.js
@@ -0,0 +1,31 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+import HelloWorld from '@/components/HelloWorld'
+import App from '../App'
+
+Vue.use(Router)
+
+export default new Router({
+  routes: [
+    {
+      path: '/helloWorld',
+      component: HelloWorld
+    },
+    {
+      path: '/',
+      component: App,
+      children: [{
+        path: '',
+        component: r => require.ensure([], () => r(require('../page/home')), 'home')
+      },
+      {
+        path: '/item',
+        component: r => require.ensure([], () => r(require('../page/item')), 'item')
+      },
+      {
+        path: '/score',
+        component: r => require.ensure([], () => r(require('../page/score')), 'score')
+      }]
+    }
+  ]
+})
