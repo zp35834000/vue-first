b8359381d35cbbc49ff408a20e5e6515895e6088
zp <zhao_4365637@163.com>
2018-04-09 16:36:33 +0800

    增加了表格一次性加载大量数据的异步相应

---
+++ src/components/datagrid/datagrid.vue
@@ -0,0 +1,29 @@
+<template>
+  <div id="">
+    <table :id='id' border="0" style="float:left">
+      
+    </table>
+  </div>
+</template>
+<script>
+export default {
+  name: "",
+  data: () => ({
+
+  }),
+  props: {
+    // 事件中心
+    bus: {
+      type: Object,
+      required: true
+    },
+    // 表格唯一标识
+    id: {
+      type: String,
+      required: true
+    }
+  }
+}
+</script>
+<style lang="scss" scoped>
+</style>
---
+++ src/components/datagrid/index.js
@@ -0,0 +1,3 @@
+import datagrid from './datagrid'
+import column from './column'
+export {datagrid,column}
